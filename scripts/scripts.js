a3fd5ad2f4ffa111ee44f5b9e1706cba787bcaed
Poo <47221891+beatle72298@users.noreply.github.com>
2021-02-19 09:01:40 -0600

    new floral bg, inserted logo, fixed nav highlight

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -94,7 +94,7 @@ $(window).scroll(function() {
     var windscroll = $(window).scrollTop();
     if (windscroll >= 200) {
         $('.site-wrapper section').each(function(i) {
-            if ($(this).position().top <= windscroll + 250){
+            if ($(this).position().top <= windscroll - 450){
                 $('nav a.active').removeClass('active');
                 $('nav a').eq(i).addClass('active');
             }
